7ba0ee4947637198aaad2769615c622256e66f94
mugurbil <mu388@nyu.edu>
2015-04-13 17:37:29 -0400

    4

--- M_AND_M.sh
+++ M_AND_M.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
+#PBS -l nodes=1:ppn=1
+#PBS -l walltime=12:00:00
+#PBS -l mem=64GB
+#PBS -N M_AND_M
 
-qsub -I -l nodes=1:ppn=1:gpus=1:titan -l mem=8GB
+cd ~
 
 git clone https://github.com/markisus/Deep-Learning-Assignment-3.git part1
 
-cd ~
 git clone https://github.com/mugurbil/deepLearningA3.git M_AND_M
 mv part1/A3_baseline.lua M_AND_M/A3_baseline.lua
 mv part1/A3_skeleton.lua M_AND_M/A3_skeleton.lua
 
 cd M_AND_M/
 
-th M_AND_M_model.lua
-./M_AND_M_model.sh
+wget https://cims.nyu.edu/~mu388/model.net
 
